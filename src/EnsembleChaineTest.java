f4abea7b0645e3a63efc0d4ecfc16f53bce2672f
aymeric131 <aymeric.ludo@gmail.com>
2021-11-09 10:32:29 -0400

    EnsembleChaineTest

--- src/EnsembleChaineTest.java
+++ src/EnsembleChaineTest.java
@@ -1,39 +1,61 @@
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class EnsembleChaineTest {
 
-    EnsembleChaine EnsembleChaine = new EnsembleChaine();
-    String str = "abc";
+    EnsembleChaine e1 = new EnsembleChaine();
 
-    @org.junit.Test
+    @Test
+    // Vrai : element pas présent et ajouté  Faux : element déja present et non ajouté
     public void ajouterTest() {
-        Boolean exist = false;
-        for (String ch : EnsembleChaine.liste) {
-            if (str.equals(ch)) {
-                exist = true;
-            } else {
-                exist = false;
-            }
-        }
-        assertFalse(exist);
+        String str1 = "abc";
+        assertTrue(e1.ajouter(str1) && e1.liste.size() == 1 && e1.liste.contains(str1));
+
+        //2 cas " Ajout dans un ensemble non vide"
+        String str2 = "123";
+        assertTrue(e1.ajouter(str2) && e1.liste.size() == 2 && e1.liste.contains(str1) && e1.liste.contains(str2));
+
+        //3 cas " Ajout d'un élément déja existant"
+        String str3 = "abc";
+        assertTrue(!e1.ajouter(str3) && e1.liste.size() == 2 && e1.liste.contains(str1) && e1.liste.contains(str2));
+
+        //Cas 4 "Ajout d'une chaine vide"
+        String str4 = "";
+        assertTrue(e1.ajouter(str4) && e1.liste.size() == 3 && e1.liste.contains(str1) && e1.liste.contains(str2) && e1.liste.contains(str4));
+
+        //Cas 5 "Ajout chaine existante et dans le désordre"
+        String str5 = "cba";
+        assertTrue(e1.ajouter(str5) && e1.liste.size() == 4 && e1.liste.contains(str1) && e1.liste.contains(str2) && e1.liste.contains(str4) && e1.liste.contains(str5));
+
+        //Cas 6 "Ajout d'une chaine null"
+        String str6 = null;
+        assertTrue(e1.ajouter(str6) && e1.liste.size() == 5 && e1.liste.contains(str1) && e1.liste.contains(str2) && e1.liste.contains(str4) && e1.liste.contains(str5) && e1.liste.contains(str6));
+
+        // Cas 7
+        String str7 = null;
+        assertTrue(!e1.ajouter(str7) && e1.liste.size() == 5 && e1.liste.contains(str1) && e1.liste.contains(str2) && e1.liste.contains(str4) && e1.liste.contains(str5) && e1.liste.contains(str6));
+
+        //Cas 8
+        String str8 = "cba";
+        assertTrue(!e1.ajouter(str8) && e1.liste.size() == 5 && e1.liste.contains(str1) && e1.liste.contains(str2) && e1.liste.contains(str4) && e1.liste.contains(str5) && e1.liste.contains(str6));
     }
 
-    @org.junit.Test
+    @Test
     public void testEquals() {
 
     }
 
-    @org.junit.Test
+    @Test
     public void union() {
     }
 
-    @org.junit.Test
+    @Test
     public void intersection() {
     }
 
-    @org.junit.Test
+    @Test
     public void unionDisjointe() {
     }
 }
